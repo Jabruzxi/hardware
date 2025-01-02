4bb2378cb14eb0118fdbbb62eb0a6cae129063aa
Jabruzxi <githiarijohn@gmail.com>
2025-01-02 13:49:17 +0300

    sorted

--- components/about.tsx
+++ components/about.tsx
@@ -1,16 +1,14 @@
-// components/About.js
 const About = () => {
-    return (
-      <section id="about" className="py-12 bg-gray-100">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-semibold mb-4">About FM Hardware</h2>
-          <p className="text-lg max-w-2xl mx-auto">
-            At FM Hardware, we are dedicated to providing top-notch hardware solutions to meet the diverse needs of our customers. Whether you're a contractor, DIY enthusiast, or business owner, we offer a wide range of high-quality tools, building materials, and accessories. Our commitment to excellence ensures that you receive reliable products and exceptional service every time you shop with us.
-          </p>
-        </div>
-      </section>
-    );
-  };
-  
-  export default About;
-  
+  return (
+    <section id="about" className="py-12 bg-gray-100">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-semibold mb-4">About FM Hardware</h2>
+        <p className="text-lg max-w-2xl mx-auto">
+          At FM Hardware, we are dedicated to providing top-notch hardware solutions to meet the diverse needs of our customers. Whether you&apos;re a contractor, DIY enthusiast, or business owner, we offer a wide range of high-quality tools, building materials, and accessories. Our commitment to excellence ensures that you receive reliable products and exceptional service every time you shop with us.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
